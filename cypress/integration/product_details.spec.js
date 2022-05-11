77d325a7e5c6180f801ed8dccaa6c671b9e7b232
Lucas Tan <sheng.x.tan@gmail.com>
2022-05-11 02:44:22 +0000

    add test for product details

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,13 @@
+describe("Jungle", () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  });
+
+  it("users can go from the home page to the product details page", () => {
+    cy.contains("Look no further than our online plant shop!");
+
+    cy.get("a[href='/products/1']").click();
+
+    cy.location('href').should("eq", "http://localhost:3000/products/1");
+  });
+});
